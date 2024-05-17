e90e30a72f77f5afde9b96bf97316bd75de1b130
KabiththananParan <KabiththananParan@gmail.com>
2024-05-17 12:10:22 +0530

    Added J

---
+++ product.js
@@ -0,0 +1,38 @@
+//Selecting side navbar
+
+var sidenav = document.getElementById("sidenav")
+var menuicon = document.getElementById("menuicon")
+var closenav = document.getElementById("close-nav")
+
+menuicon.addEventListener("click",function(){
+    sidenav.style.right=0
+})
+
+closenav.addEventListener("click",function(){
+    sidenav.style.right="-50%"
+})
+
+//Product search funcationality
+
+var productcontainer = document.getElementById("product-container")
+
+var search = document.getElementById("search")
+var productlist = productcontainer.querySelectorAll("div")
+
+search.addEventListener("keyup",function(){
+    var enteredvalue = this.value.toUpperCase()
+
+    for(var count=0; count<productlist.length; count=count+1)
+        {
+
+            var productname = productlist[count].querySelector("h1").textContent
+
+            if(productname.toUpperCase().indexOf(enteredvalue)<0)
+            {
+                productlist[count].style.display="none"
+            }
+            else{
+                productlist[count].style.display="block"
+            }
+        }
+})
